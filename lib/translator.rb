2ab1f02e9045deea475d83eca3ee0122b2490d02
Finn Thye <piqueen@gmail.com>
2019-08-11 21:17:26 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,31 @@
-# require modules here
+require "yaml"
+require "pry"
 
-def load_library
-  # code goes here
+def load_library(path)
+  emoticons = YAML.load_file(path)
+  actions = {}
+  actions[:get_meaning] = {}
+  actions[:get_emoticon] = {}
+  actions
+  #binding.pry
 end
+
+=begin
+  emoticons
+=> {"angel"=>["O:)", "☜(⌒▽⌒)☞"],
+ "angry"=>[">:(", "ヽ(ｏ`皿′ｏ)ﾉ"],
+ "bored"=>[":O", "(ΘεΘ;)"],
+ "confused"=>["%)", "(゜.゜)"],
+ "embarrased"=>[":$", "(#^.^#)"],
+ "fish"=>["><>", ">゜))))彡"],
+ "glasses"=>["8D", "(^0_0^)"],
+ "grinning"=>["=D", "(￣ー￣)"],
+ "happy"=>[":)", "(＾ｖ＾)"],
+ "kiss"=>[":*", "(*^3^)/~☆"],
+ "sad"=>[":'(", "(Ｔ▽Ｔ)"],
+ "surprised"=>[":o", "o_O"],
+ "wink"=>[";)", "(^_-)"]}
+=end
 
 def get_japanese_emoticon
   # code goes here
